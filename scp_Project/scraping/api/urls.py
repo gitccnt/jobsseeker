2ab55addd785ca5fd48b10a3b62b0385bc62e4ae
gitccnt <amznwsccnt@gmail.com>
2023-11-08 21:53:37 +0200

    clear

--- scp_Project/scraping/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .views import *
-
-router = routers.DefaultRouter()
-router.register('languages', LanguageViewSet)
-router.register('vacancy', VacancyViewSet)
-urlpatterns = router.urls
